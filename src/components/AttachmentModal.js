0e67da8c130994f80dad4f91f1e1008b41fa8bde
Carlos Martins <cmartins@expensify.com>
2023-09-05 12:33:00 -0600

    add replace callback

--- src/components/AttachmentModal.js
+++ src/components/AttachmentModal.js
@@ -29,6 +29,8 @@ import useNativeDriver from '../libs/useNativeDriver';
 import tryResolveUrlFromApiRoot from '../libs/tryResolveUrlFromApiRoot';
 import * as Expensicons from './Icon/Expensicons';
 import useWindowDimensions from '../hooks/useWindowDimensions';
+import Navigation from '../libs/Navigation/Navigation';
+import ROUTES from '../ROUTES';
 
 /**
  * Modal render prop component that exposes modal launching triggers that can be used
@@ -356,7 +358,10 @@ function AttachmentModal(props) {
                         {
                             icon: Expensicons.Camera,
                             text: props.translate('common.replace'),
-                            onSelected: () => {}, // TODO
+                            onSelected: () => {
+                                closeModal();
+                                Navigation.navigate(ROUTES.getEditRequestRoute(props.report.reportID, 'amount'));
+                            },
                         },
                         {
                             icon: Expensicons.Download,
