c5f22aa2748d139771a46fe01a1ccd6eb64d5b2a
Carlos Martins <cmartins@expensify.com>
2023-09-05 16:55:09 -0600

    add onyx back, add dropui provider

--- src/pages/EditRequestPage.js
+++ src/pages/EditRequestPage.js
@@ -184,6 +184,8 @@ export default compose(
         report: {
             key: ({route}) => `${ONYXKEYS.COLLECTION.REPORT}${route.params.threadReportID}`,
         },
+    }),
+    withOnyx({
         parentReport: {
             key: ({report}) => `${ONYXKEYS.COLLECTION.REPORT}${report ? report.parentReportID : '0'}`,
         },
